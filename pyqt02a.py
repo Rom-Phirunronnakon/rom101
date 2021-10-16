876118f9dd67e624fb008e2ce393946b10f44774
Rom Phirunronnakon <64011598@kmitl.ac.th>
2021-10-16 10:32:11 +0700

    add helloworld.py

---
+++ pyqt02a.py
@@ -0,0 +1,78 @@
+from PyQt5.QtWidgets import *
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        # Your code here
+
+        self.layout1 = QVBoxLayout()
+        self.setLayout(self.layout1)
+
+        self.layout2 = QHBoxLayout()
+        self.label1 = QLabel('Enter x')
+        self.lineEditX = QLineEdit()
+        self.layout2.addWidget(self.label1)
+        self.layout2.addWidget(self.lineEditX)
+        self.layout1.addLayout(self.layout2)
+
+        self.layout3 = QHBoxLayout()
+        self.label2 = QLabel('Enter y')
+        self.lineEditY = QLineEdit()
+        self.layout3.addWidget(self.label2)
+        self.layout3.addWidget(self.lineEditY)
+        self.layout1.addLayout(self.layout3)
+        
+        self.buttonAdd = QPushButton('+')
+        self.buttonAdd.clicked.connect(self.actionAdd)
+        self.layout1.addWidget(self.buttonAdd)
+
+        self.buttonMinus = QPushButton('-')
+        self.buttonMinus.clicked.connect(self.actionMinus)
+        self.layout1.addWidget(self.buttonMinus)
+
+        self.buttonTimes = QPushButton('*')
+        self.buttonTimes.clicked.connect(self.actionTimes)
+        self.layout1.addWidget(self.buttonTimes)
+
+        self.buttonDiv = QPushButton('/')
+        self.buttonDiv.clicked.connect(self.actionDiv)
+        self.layout1.addWidget(self.buttonDiv)
+
+        self.layout4 = QHBoxLayout()
+        self.label3 = QLabel('result =')
+        self.labelResult = QLabel()
+        self.layout4.addWidget(self.label3)
+        self.layout4.addWidget(self.labelResult)
+        self.layout1.addLayout(self.layout4)
+
+
+    def actionAdd(self):
+        x = int(self.lineEditX.text())
+        y = int(self.lineEditY.text())
+        z = x+y
+        self.labelResult.setText(str(z))
+
+    def actionMinus(self):
+        x = int(self.lineEditX.text())
+        y = int(self.lineEditY.text())
+        z = x-y
+        self.labelResult.setText(str(z))
+    
+    def actionTimes(self):
+        x = int(self.lineEditX.text())
+        y = int(self.lineEditY.text())
+        z = x*y
+        self.labelResult.setText(str(z))
+    
+    def actionDiv(self):
+        x = int(self.lineEditX.text())
+        y = int(self.lineEditY.text())
+        if y != 0:
+            z = x/y
+            self.labelResult.setText(str(z))
+        
+    
+app = QApplication([])
+myWindow = MyWindow()
+myWindow.show()
+app.exec()
